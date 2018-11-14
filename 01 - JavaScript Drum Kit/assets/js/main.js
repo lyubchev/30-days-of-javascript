e74a1855fcf188dbcb20a6fd2af69601453e981c
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-11-14 20:36:36 +0200

    Finish Drum Kit

---
+++ 01 - JavaScript Drum Kit/assets/js/main.js
@@ -0,0 +1,21 @@
+handleKeyDown = e => {
+
+  // get the correct audio file based on pressed key
+  const audio = document.querySelector(`audio[data-key="${e.key.toLowerCase()}"]`);
+
+  // get the correct div element corresponding to the sound based on pressed key
+  const key = document.querySelector(`.key[data-key="${e.key.toLowerCase()}"]`);
+
+  if (audio && key) {
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+
+    // listen for transionend so styles can be removed after the transition is done
+    key.addEventListener('transitionend', () =>
+      key.classList.remove('playing')
+    );
+  }
+};
+
+window.addEventListener('keydown', handleKeyDown);
