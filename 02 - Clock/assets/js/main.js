14acf538fd65642971b98b853f8ca0fe91a88f0f
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-11-16 18:11:36 +0200

    Fix timing offset, Display current time in h1

--- 02 - Clock/assets/js/main.js
+++ 02 - Clock/assets/js/main.js
@@ -1,28 +1,27 @@
 const secHand = document.querySelector('.sec-hand');
 const minHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
-const currentTime = new Date();
+const currentTimeDisplay = document.querySelector('#current-time');
+let currentTime = new Date();
 let seconds = currentTime.getSeconds();
 let minutes = currentTime.getMinutes();
 let hours = currentTime.getHours();
 
 updateDate = () => {
- 
-  let secondsDegrees = 0;
-  let minutesDegrees = 0;
-  let hourDegrees = 0;
+  currentTime = new Date();
+  currentTimeDisplay.textContent = currentTime.toLocaleTimeString();
 
-  seconds++;
-  secondsDegrees += (seconds / 60) * 360 + 90;
-  minutes = seconds % 60 == 0 ? minutes + 1 : minutes;
-  minutesDegrees += (minutes / 60) * 360 + 90;
-  hours = minutes % 60 == 0 ? hours + 1 : hours;
-  hourDegrees += (hours / 12) * 360 + 90;
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  const hourDegrees = (hours / 12) * 360 + 90;
 
   secHand.style.transform = `rotate(${secondsDegrees}deg)`;
   minHand.style.transform = `rotate(${minutesDegrees}deg)`;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
+  seconds++;
+  minutes = seconds % 60 == 0 ? minutes + 1 : minutes;
+  hours = minutes % 60 == 0 && minutes != 0 ? hours + 1 : hours;
 };
 
 // initial update
